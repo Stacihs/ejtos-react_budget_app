5a65e406bba3209f5634b6b61c7eafa1ae7a1a89
Stacihs <staciholmessegarra@gmail.com>
2024-03-10 03:36:15 -0400

    Added a dispatch action to the Budget component and updated the AppContext.js accordingly

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -64,6 +64,12 @@ export const AppReducer = (state, action) => {
             return {
                 ...state,
             };
+        case 'UPDATE_BUDGET':
+            action.type = "DONE";
+            state.budget = action.payload;
+            return {
+                ...state,
+            };
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
